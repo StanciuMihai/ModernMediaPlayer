0e229cdd41e0e488ad5bb33b418b37213342beb3
Stanciu Mihai <misu_sergiu@yahoo.com>
2021-05-09 23:35:52 +0300

    Added extra options to visualisation menu

--- Modern UI audio player/VisualisationSelector.Designer.cs
+++ Modern UI audio player/VisualisationSelector.Designer.cs
@@ -33,6 +33,20 @@ namespace Modern_UI_audio_player
             this.btnVis1 = new System.Windows.Forms.Button();
             this.btnVis2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnVis3 = new System.Windows.Forms.Button();
+            this.btnVis4 = new System.Windows.Forms.Button();
+            this.btnVis8 = new System.Windows.Forms.Button();
+            this.btnVis7 = new System.Windows.Forms.Button();
+            this.btnVis6 = new System.Windows.Forms.Button();
+            this.btnVis5 = new System.Windows.Forms.Button();
+            this.btnVis12 = new System.Windows.Forms.Button();
+            this.btnVis11 = new System.Windows.Forms.Button();
+            this.btnVis10 = new System.Windows.Forms.Button();
+            this.btnVis9 = new System.Windows.Forms.Button();
+            this.btnVis16 = new System.Windows.Forms.Button();
+            this.btnVis15 = new System.Windows.Forms.Button();
+            this.btnVis14 = new System.Windows.Forms.Button();
+            this.btnVis13 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnVis1
@@ -41,9 +55,10 @@ namespace Modern_UI_audio_player
             this.btnVis1.FlatAppearance.BorderSize = 0;
             this.btnVis1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVis1.Image = ((System.Drawing.Image)(resources.GetObject("btnVis1.Image")));
-            this.btnVis1.Location = new System.Drawing.Point(104, 105);
+            this.btnVis1.Location = new System.Drawing.Point(73, 86);
+            this.btnVis1.Margin = new System.Windows.Forms.Padding(1);
             this.btnVis1.Name = "btnVis1";
-            this.btnVis1.Size = new System.Drawing.Size(142, 80);
+            this.btnVis1.Size = new System.Drawing.Size(132, 75);
             this.btnVis1.TabIndex = 0;
             this.btnVis1.UseVisualStyleBackColor = true;
             this.btnVis1.Click += new System.EventHandler(this.btnVis1_Click);
@@ -54,9 +69,10 @@ namespace Modern_UI_audio_player
             this.btnVis2.FlatAppearance.BorderSize = 0;
             this.btnVis2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVis2.Image = ((System.Drawing.Image)(resources.GetObject("btnVis2.Image")));
-            this.btnVis2.Location = new System.Drawing.Point(243, 105);
+            this.btnVis2.Location = new System.Drawing.Point(212, 86);
+            this.btnVis2.Margin = new System.Windows.Forms.Padding(1);
             this.btnVis2.Name = "btnVis2";
-            this.btnVis2.Size = new System.Drawing.Size(142, 80);
+            this.btnVis2.Size = new System.Drawing.Size(132, 75);
             this.btnVis2.TabIndex = 1;
             this.btnVis2.UseVisualStyleBackColor = true;
             this.btnVis2.Click += new System.EventHandler(this.btnVis2_Click);
@@ -67,11 +83,193 @@ namespace Modern_UI_audio_player
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
-            this.label1.Location = new System.Drawing.Point(219, 36);
+            this.label1.Location = new System.Drawing.Point(219, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(278, 25);
             this.label1.TabIndex = 20;
             this.label1.Text = "VISUALISATION SELECTOR";
+            // 
+            // btnVis3
+            // 
+            this.btnVis3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis3.FlatAppearance.BorderSize = 0;
+            this.btnVis3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis3.Image = ((System.Drawing.Image)(resources.GetObject("btnVis3.Image")));
+            this.btnVis3.Location = new System.Drawing.Point(346, 86);
+            this.btnVis3.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis3.Name = "btnVis3";
+            this.btnVis3.Size = new System.Drawing.Size(132, 75);
+            this.btnVis3.TabIndex = 21;
+            this.btnVis3.UseVisualStyleBackColor = true;
+            // 
+            // btnVis4
+            // 
+            this.btnVis4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis4.FlatAppearance.BorderSize = 0;
+            this.btnVis4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis4.Image = ((System.Drawing.Image)(resources.GetObject("btnVis4.Image")));
+            this.btnVis4.Location = new System.Drawing.Point(480, 86);
+            this.btnVis4.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis4.Name = "btnVis4";
+            this.btnVis4.Size = new System.Drawing.Size(132, 75);
+            this.btnVis4.TabIndex = 22;
+            this.btnVis4.UseVisualStyleBackColor = true;
+            // 
+            // btnVis8
+            // 
+            this.btnVis8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis8.FlatAppearance.BorderSize = 0;
+            this.btnVis8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis8.Image = ((System.Drawing.Image)(resources.GetObject("btnVis8.Image")));
+            this.btnVis8.Location = new System.Drawing.Point(480, 157);
+            this.btnVis8.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis8.Name = "btnVis8";
+            this.btnVis8.Size = new System.Drawing.Size(132, 75);
+            this.btnVis8.TabIndex = 26;
+            this.btnVis8.UseVisualStyleBackColor = true;
+            // 
+            // btnVis7
+            // 
+            this.btnVis7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis7.FlatAppearance.BorderSize = 0;
+            this.btnVis7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis7.Image = ((System.Drawing.Image)(resources.GetObject("btnVis7.Image")));
+            this.btnVis7.Location = new System.Drawing.Point(346, 157);
+            this.btnVis7.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis7.Name = "btnVis7";
+            this.btnVis7.Size = new System.Drawing.Size(132, 75);
+            this.btnVis7.TabIndex = 25;
+            this.btnVis7.UseVisualStyleBackColor = true;
+            // 
+            // btnVis6
+            // 
+            this.btnVis6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis6.FlatAppearance.BorderSize = 0;
+            this.btnVis6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis6.Image = ((System.Drawing.Image)(resources.GetObject("btnVis6.Image")));
+            this.btnVis6.Location = new System.Drawing.Point(212, 157);
+            this.btnVis6.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis6.Name = "btnVis6";
+            this.btnVis6.Size = new System.Drawing.Size(132, 75);
+            this.btnVis6.TabIndex = 24;
+            this.btnVis6.UseVisualStyleBackColor = true;
+            // 
+            // btnVis5
+            // 
+            this.btnVis5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis5.FlatAppearance.BorderSize = 0;
+            this.btnVis5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis5.Image = ((System.Drawing.Image)(resources.GetObject("btnVis5.Image")));
+            this.btnVis5.Location = new System.Drawing.Point(73, 157);
+            this.btnVis5.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis5.Name = "btnVis5";
+            this.btnVis5.Size = new System.Drawing.Size(132, 75);
+            this.btnVis5.TabIndex = 23;
+            this.btnVis5.UseVisualStyleBackColor = true;
+            // 
+            // btnVis12
+            // 
+            this.btnVis12.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis12.FlatAppearance.BorderSize = 0;
+            this.btnVis12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis12.Image = ((System.Drawing.Image)(resources.GetObject("btnVis12.Image")));
+            this.btnVis12.Location = new System.Drawing.Point(480, 234);
+            this.btnVis12.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis12.Name = "btnVis12";
+            this.btnVis12.Size = new System.Drawing.Size(132, 75);
+            this.btnVis12.TabIndex = 30;
+            this.btnVis12.UseVisualStyleBackColor = true;
+            // 
+            // btnVis11
+            // 
+            this.btnVis11.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis11.FlatAppearance.BorderSize = 0;
+            this.btnVis11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis11.Image = ((System.Drawing.Image)(resources.GetObject("btnVis11.Image")));
+            this.btnVis11.Location = new System.Drawing.Point(346, 234);
+            this.btnVis11.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis11.Name = "btnVis11";
+            this.btnVis11.Size = new System.Drawing.Size(132, 75);
+            this.btnVis11.TabIndex = 29;
+            this.btnVis11.UseVisualStyleBackColor = true;
+            // 
+            // btnVis10
+            // 
+            this.btnVis10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis10.FlatAppearance.BorderSize = 0;
+            this.btnVis10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis10.Image = ((System.Drawing.Image)(resources.GetObject("btnVis10.Image")));
+            this.btnVis10.Location = new System.Drawing.Point(212, 234);
+            this.btnVis10.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis10.Name = "btnVis10";
+            this.btnVis10.Size = new System.Drawing.Size(132, 75);
+            this.btnVis10.TabIndex = 28;
+            this.btnVis10.UseVisualStyleBackColor = true;
+            // 
+            // btnVis9
+            // 
+            this.btnVis9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis9.FlatAppearance.BorderSize = 0;
+            this.btnVis9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis9.Image = ((System.Drawing.Image)(resources.GetObject("btnVis9.Image")));
+            this.btnVis9.Location = new System.Drawing.Point(73, 234);
+            this.btnVis9.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis9.Name = "btnVis9";
+            this.btnVis9.Size = new System.Drawing.Size(132, 75);
+            this.btnVis9.TabIndex = 27;
+            this.btnVis9.UseVisualStyleBackColor = true;
+            // 
+            // btnVis16
+            // 
+            this.btnVis16.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis16.FlatAppearance.BorderSize = 0;
+            this.btnVis16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis16.Image = ((System.Drawing.Image)(resources.GetObject("btnVis16.Image")));
+            this.btnVis16.Location = new System.Drawing.Point(480, 311);
+            this.btnVis16.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis16.Name = "btnVis16";
+            this.btnVis16.Size = new System.Drawing.Size(132, 75);
+            this.btnVis16.TabIndex = 34;
+            this.btnVis16.UseVisualStyleBackColor = true;
+            // 
+            // btnVis15
+            // 
+            this.btnVis15.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis15.FlatAppearance.BorderSize = 0;
+            this.btnVis15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis15.Image = ((System.Drawing.Image)(resources.GetObject("btnVis15.Image")));
+            this.btnVis15.Location = new System.Drawing.Point(346, 311);
+            this.btnVis15.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis15.Name = "btnVis15";
+            this.btnVis15.Size = new System.Drawing.Size(132, 75);
+            this.btnVis15.TabIndex = 33;
+            this.btnVis15.UseVisualStyleBackColor = true;
+            // 
+            // btnVis14
+            // 
+            this.btnVis14.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis14.FlatAppearance.BorderSize = 0;
+            this.btnVis14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis14.Image = ((System.Drawing.Image)(resources.GetObject("btnVis14.Image")));
+            this.btnVis14.Location = new System.Drawing.Point(212, 311);
+            this.btnVis14.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis14.Name = "btnVis14";
+            this.btnVis14.Size = new System.Drawing.Size(132, 75);
+            this.btnVis14.TabIndex = 32;
+            this.btnVis14.UseVisualStyleBackColor = true;
+            // 
+            // btnVis13
+            // 
+            this.btnVis13.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVis13.FlatAppearance.BorderSize = 0;
+            this.btnVis13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVis13.Image = ((System.Drawing.Image)(resources.GetObject("btnVis13.Image")));
+            this.btnVis13.Location = new System.Drawing.Point(73, 311);
+            this.btnVis13.Margin = new System.Windows.Forms.Padding(1);
+            this.btnVis13.Name = "btnVis13";
+            this.btnVis13.Size = new System.Drawing.Size(132, 75);
+            this.btnVis13.TabIndex = 31;
+            this.btnVis13.UseVisualStyleBackColor = true;
             // 
             // VisualisationSelector
             // 
@@ -79,6 +277,20 @@ namespace Modern_UI_audio_player
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
             this.ClientSize = new System.Drawing.Size(684, 431);
+            this.Controls.Add(this.btnVis16);
+            this.Controls.Add(this.btnVis15);
+            this.Controls.Add(this.btnVis14);
+            this.Controls.Add(this.btnVis13);
+            this.Controls.Add(this.btnVis12);
+            this.Controls.Add(this.btnVis11);
+            this.Controls.Add(this.btnVis10);
+            this.Controls.Add(this.btnVis9);
+            this.Controls.Add(this.btnVis8);
+            this.Controls.Add(this.btnVis7);
+            this.Controls.Add(this.btnVis6);
+            this.Controls.Add(this.btnVis5);
+            this.Controls.Add(this.btnVis4);
+            this.Controls.Add(this.btnVis3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVis2);
             this.Controls.Add(this.btnVis1);
@@ -94,5 +306,19 @@ namespace Modern_UI_audio_player
         private System.Windows.Forms.Button btnVis1;
         private System.Windows.Forms.Button btnVis2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVis3;
+        private System.Windows.Forms.Button btnVis4;
+        private System.Windows.Forms.Button btnVis8;
+        private System.Windows.Forms.Button btnVis7;
+        private System.Windows.Forms.Button btnVis6;
+        private System.Windows.Forms.Button btnVis5;
+        private System.Windows.Forms.Button btnVis12;
+        private System.Windows.Forms.Button btnVis11;
+        private System.Windows.Forms.Button btnVis10;
+        private System.Windows.Forms.Button btnVis9;
+        private System.Windows.Forms.Button btnVis16;
+        private System.Windows.Forms.Button btnVis15;
+        private System.Windows.Forms.Button btnVis14;
+        private System.Windows.Forms.Button btnVis13;
     }
 }
